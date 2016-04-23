7b8c167cfa202ee7de843fa75c4a5839cf8d4fc8
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-22 23:22:58 -0700

    Add header.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+import GiftedSpinner from 'react-native-gifted-spinner';
+
+export default class header extends Component {
