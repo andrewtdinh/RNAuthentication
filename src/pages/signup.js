437b437674c9d25cdf2a617f6801d5bd4605a46d
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-23 13:59:57 -0700

    Add component class with constructor

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -18,3 +18,16 @@ import Firebase from 'firebase';
 let app = new Firebase("YOUR-FIREBASE-APP-URL");
 
 import styles from '../styles/common-styles.js';
+
+export default class signup extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loaded: true,
+      email: '',
+      password: ''
+    };
+  }
+}
