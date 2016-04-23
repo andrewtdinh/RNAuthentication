c845205f2d35e99164fafd5c2abdee7a5077fe5a
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-23 14:02:51 -0700

    Add signup method

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -30,4 +30,43 @@ export default class signup extends Component {
       password: ''
     };
   }
+  signup(){
+
+    this.setState({
+      loaded: false
+    });
+
+    app.createUser({
+      'email': this.state.email,
+      'password': this.state.password
+    }, (error, userData) => {
+
+      if(error){
+        switch(error.code){
+
+          case "EMAIL_TAKEN":
+            alert("The new user account cannot be created because the email is already in use.");
+          break;
+
+          case "INVALID_EMAIL":
+            alert("The specified email is not a valid email.");
+          break;
+
+          default:
+            alert("Error creating user:");
+        }
+
+      }else{
+        alert('Your account was created!');
+      }
+
+      this.setState({
+        email: '',
+        password: '',
+        loaded: true
+      });
+
+    });
+
+  }
 }
