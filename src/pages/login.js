561ca97e81dd492004ae45c43c00f31439c329f5
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-24 20:19:27 -0700

    Perform necessary imports

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,3 +8,15 @@ import React, {
   View,
   AsyncStorage
 } from 'react-native';
+
+import Button from '../components/button';
+import Header from '../components/header';
+
+import Signup from './signup';
+import Account from './account';
+
+import Firebase from 'firebase';
+
+let app = new Firebase("YOUR-FIREBASE-APP-URL");
+
+import styles from '../styles/common-styles.js';
