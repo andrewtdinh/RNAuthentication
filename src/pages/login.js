2e249f11ad0412920342d81e5d6f0e74a69000df
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-24 20:22:37 -0700

    Add render function

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,7 +22,6 @@ let app = new Firebase("YOUR-FIREBASE-APP-URL");
 import styles from '../styles/common-styles.js';
 
 export default class login extends Component {
-
   constructor(props){
     super(props);
 
@@ -31,5 +30,40 @@ export default class login extends Component {
       password: '',
       loaded: true
     }
+  }
+  
+  render(){
+    return (
+      <View style={styles.container}>
+        <Header text="Login" loaded={this.state.loaded} />
+        <View style={styles.body}>
+          <TextInput
+            style={styles.textinput}
+            onChangeText={(text) => this.setState({email: text})}
+            value={this.state.email}
+            placeholder={"Email Address"}
+          />
+          <TextInput
+            style={styles.textinput}
+            onChangeText={(text) => this.setState({password: text})}
+            value={this.state.password}
+            secureTextEntry={true}
+            placeholder={"Password"}
+          />
+
+          <Button
+            text="Login"
+            onpress={this.login.bind(this)}
+            button_styles={styles.primary_button}
+            button_text_styles={styles.primary_button_text} />
+
+          <Button
+            text="New here?"
+            onpress={this.goToSignup.bind(this)}
+            button_styles={styles.transparent_button}
+            button_text_styles={styles.transparent_button_text} />
+        </View>
+      </View>
+    );
   }
 }
