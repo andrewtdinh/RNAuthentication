cabd9c26c57a6e5dfedc24a34e98747651fe50c3
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-25 17:36:19 -0700

    More import statements and define firebase variable

--- src/pages/account.js
+++ src/pages/account.js
@@ -9,3 +9,14 @@ import React, {
   Image,
   AsyncStorage
 } from 'react-native';
+
+import Button from '../components/button';
+import Header from '../components/header';
+
+import Login from './login';
+
+import styles from '../styles/common-styles.js';
+
+import Firebase from 'firebase';
+
+let app = new Firebase("YOUR-FIREBASE-APP-URL");
