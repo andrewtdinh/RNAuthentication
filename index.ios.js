ac539ddef5a7061f9abef1d1958b11ba4eccc0a6
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-28 23:47:05 -0700

    Import the necessary components into index.ios

--- index.ios.js
+++ index.ios.js
@@ -7,3 +7,14 @@ import React, {
   Navigator,
   AsyncStorage
 } from 'react-native';
+
+import Signup from './src/pages/signup';
+import Account from './src/pages/account';
+
+import Header from './src/components/header';
+
+import Firebase from 'firebase';
+
+let app = new Firebase("YOUR-FIREBASE-APP-URL");
+
+import styles from './src/styles/common-styles.js';
