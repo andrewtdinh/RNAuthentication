687a859a9584c4d0e65264e1e65f1ac59803223a
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-30 21:36:17 -0700

    Update Android main component

--- index.android.js
+++ index.android.js
@@ -20,20 +20,56 @@ let app = new Firebase("https://fbauthenticate.firebaseio.com/");
 import styles from './src/styles/common-styles.js';
 
 class RNFB_Authenticate extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+
+  constructor(props){
+    super(props);
+    this.state = {
+      component: null,
+      loaded: false
+    };
+  }
+
+  componentWillMount(){
+    AsyncStorage.getItem('user_data').then((user_data_json) => {
+      let user_data = JSON.parse(user_data_json);
+      let component = {component: Signup};
+      if(user_data != null){
+        app.authWithCustomToken(user_data.token, (error, authData) => {
+          if(error){
+            this.setState(component);
+          }else{
+            this.setState({component: Account});
+          }
+        });
+      }else{
+        this.setState(component);
+      }
+    });
+  }
+
+  render(){
+    if(this.state.component){
+      return (
+        <Navigator
+          initialRoute={{component: this.state.component}}
+          configureScene={() => {
+            return Navigator.SceneConfigs.FloatFromRight;
+          }}
+          renderScene={(route, navigator) => {
+            if(route.component){
+              return React.createElement(route.component, { navigator });
+            }
+          }}
+        />
+      );
+    }else{
+      return (
+        <View style={styles.container}>
+          <Header text="React Native Firebase Auth" loaded={this.state.loaded} />
+          <View style={styles.body}></View>
+        </View>
+      );
+    }
   }
 }
 
